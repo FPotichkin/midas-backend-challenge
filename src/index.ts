488d1ea411f3156c1e2b7c4ffffffc6266be06a7
fpotichkin <fpotichkin@gmail.com>
2022-03-29 10:58:44 -0300

    Basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+import express from "express";
+import routes from './routes/index'
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use('/api', routes)
+
+app.listen(8000,()=>{
+    console.log("Listening on port 8000")
+})
+
+
